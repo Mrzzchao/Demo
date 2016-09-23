67db0221b0e0850f70ce21f81e61a59722cec374
Mrzzchao <mrzzchao@foxmail.com>
2016-09-23 23:58:48 +0800

    2048

---
+++ HTML5/WebSocket/support/index.js
@@ -0,0 +1,9 @@
+(function() {
+    var spt = document.getElementById('support');
+    if(window.WebSocket) {
+        spt.innerHTML = "support HTML5 WebSocket";
+    }
+    else {
+        spt.innerHTML = "not support HTML5 WebSocket";
+    }
+})();
